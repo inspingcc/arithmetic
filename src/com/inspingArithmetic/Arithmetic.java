b6f34c800e193d8a8f921eece77713a20331e216
houshanping <273371337@qq.com>
2019-10-22 00:09:34 +0800

    leetcode no.4

--- src/com/inspingArithmetic/Arithmetic.java
+++ src/com/inspingArithmetic/Arithmetic.java
@@ -122,52 +122,42 @@ public class Arithmetic {
         // 59. 螺旋矩阵 II
 //        System.out.println(c.generateMatrix(3));
         // 4. 寻找两个有序数组的中位数
-
+        System.out.println(c.findMedianSortedArrays(new int[]{1, 2}, new int[]{3,4}));
     }
 
     // 4. 寻找两个有序数组的中位数
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
         int m = nums1.length;
         int n = nums2.length;
-        if (m > n) {
-            int[] temp = nums1;
-            nums1 = nums2;
-            nums2 = temp;
-            int tmp = m;
-            m = n;
-            n = tmp;
-        }
-        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        int iMin = 0, iMax = m;
         while (iMin <= iMax) {
             int i = (iMin + iMax) / 2;
-            int j = halfLen - i;
-            if (i < iMax && nums2[j - 1] > nums1[i]) {
-                iMin = i + 1; // i 太小了
-            } else if (i > iMin && nums1[i - 1] > nums2[j]) {
-                iMax = i - 1; // i 太大了
-            } else { // i 刚刚好
-                int maxLeft = 0;
+            int j = (m + n + 1) / 2 - i;
+            if (j != 0 && i != m && nums1[i] < nums2[j - 1]) {
+                iMin = i + 1;   // nums1的后半部分
+            } else if (i != 0 && j != n && nums1[i - 1] > nums2[j]) {
+                iMax = i - 1;   // nums1的前半部分
+            } else {
+                // 达成,处理边界情况
+                int leftMax = 0; // 左边最大
                 if (i == 0) {
-                    maxLeft = nums2[j - 1];
+                    leftMax = nums2[j - 1];
                 } else if (j == 0) {
-                    maxLeft = nums1[i - 1];
+                    leftMax = nums1[i - 1];
                 } else {
-                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                    leftMax = Math.max(nums1[i - 1], nums2[j - 1]);
                 }
-                if ((m + n) % 2 == 1) {
-                    return maxLeft;
-                }
-
-                int minRight = 0;
+                if (((m + n) & 1) == 1) return leftMax;
+                int rightMin = 0;
                 if (i == m) {
-                    minRight = nums2[j];
+                    rightMin = nums2[j];
                 } else if (j == n) {
-                    minRight = nums1[i];
+                    rightMin = nums1[i];
                 } else {
-                    minRight = Math.min(nums2[j], nums1[i]);
+                    rightMin = Math.min(nums1[i], nums2[j]);
                 }
-
-                return (maxLeft + minRight) / 2.0;
+                return (leftMax + rightMin) / 2.0;
             }
         }
         return 0.0;
